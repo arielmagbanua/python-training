5deaa476e88a06c198a8dc08a96fe4f2cada60d8
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-11-03 22:39:08 +0800

    Added skewing starter code and image.

---
+++ CS101/skewing.py
@@ -0,0 +1,29 @@
+from math import *
+from cs1media import *
+
+
+img = load_picture("images/test.png")
+w,h = img.size()
+#new_w,new_h = 
+
+
+#----------------------------------------------------------#
+#
+# Function skew
+#
+# Input:
+#		img: loaded image
+#		direction: vertical or horizontal
+#		angle: -89 to 89 degrees
+#
+# Output:
+#		new_img: skewed image
+#		print “Wrong input!!!” if inputs are not in range
+#
+#----------------------------------------------------------#
+
+def skew(img, direction, angle):
+	#implement here
+
+	# new_img.show()
+    pass
