0702e5d48df5112c6bbaab9534de023dfc69f4b9
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-11-03 22:27:02 +0800

    Added decimal to any file conversion.

---
+++ CS101/dec_to_any.py
@@ -0,0 +1,45 @@
+import math
+
+#--------------------------------------------------------------#
+#
+# Function dec_to_any
+#
+# Input:
+#		n: number to convert
+#		radix: radix to use
+#
+# Output:
+#		new_num: converted number
+#
+#--------------------------------------------------------------#
+def dec_to_any_list(n,radix):
+    pass
+
+
+def dec_to_any_string(n, radix):
+    pass
+
+#--------------------------------------------------------------#
+#
+# Function main
+#
+# This is the driver function
+# Do commandline input/output and formatting here
+#
+#		Example:
+#		Enter a number: 61
+#		Enter a radix: 16
+#		61 in base 10 is 3D in base 16
+#
+#
+# Just digits, no gap or another character
+# 123 (O)    ‘1’ ‘2’ ‘3’ (x)
+# “Wrong input!!!” if inputs are not in range
+# You must use the formatting operator % (TAs will check!!!)
+
+#--------------------------------------------------------------#
+
+def main():
+    pass
+
+main()
