3d854b132fceeed7645c5b87272933c9b43c16aa
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-11-04 23:42:09 +0800

    Finished the number conversion.

--- CS101/dec_to_any.py
+++ CS101/dec_to_any.py
@@ -1,5 +1,7 @@
 import math
 
+number_letters = 'ABCDEF'
+
 #--------------------------------------------------------------#
 #
 # Function dec_to_any
@@ -12,12 +14,50 @@ import math
 #		new_num: converted number
 #
 #--------------------------------------------------------------#
-def dec_to_any_list(n,radix):
-    pass
+def dec_to_any_list(n, radix):
+    result = []
+    quotient = n
 
+    if n < radix:
+        result.append(n)
+
+    while quotient >= radix:
+        quotien_result = math.floor(quotient / radix)
+        remainder = quotient % radix
+
+        quotient = quotien_result
+
+        result.append(remainder)
+
+        if quotient < radix:
+            result.append(quotient)
+    
+    # reverse the order
+    result = result[::-1]
+
+    # replace the numbers with letters depending on radix
+    new_result = []
+    for num in result:
+        val = num
+
+        if 10 <= num < radix:
+            # compute for the right index and get the equivalent letter
+            val = number_letters[num - 10]
+        
+        new_result.append(val)
+
+    return new_result
 
 def dec_to_any_string(n, radix):
-    pass
+    # get the list form of conversion
+    converted = dec_to_any_list(n, radix)
+    
+    # using list comprehension cast / convert each item to string 
+    converted = [str(item) for item in converted]
+
+    # join the list into one string and return it
+    converted_number = ''.join(converted)
+    return converted_number
 
 #--------------------------------------------------------------#
 #
@@ -40,6 +80,19 @@ def dec_to_any_string(n, radix):
 #--------------------------------------------------------------#
 
 def main():
-    pass
+    n = int(input('Enter a number: '))
+    if (n < 0):
+        print('Wrong Input!!!')
+        return
+
+    radix = int(input('Enter a radix: '))
+
+    if (radix > 16):
+        print('Wrong Input!!!')
+        return
+
+    converted_number = dec_to_any_string(n, radix)
+
+    print('%d in base 10 is %s in base %d' % (n, converted_number, radix))
 
 main()
