dcf257687c76da79e9d65f64e8f8c9cff71cf49d
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-11-13 20:09:48 +0800

    Code clean-up.

--- CS101/coordinate_information.py
+++ CS101/coordinate_information.py
@@ -1,48 +1,40 @@
 import re
 
-def read_average_lat_long_countries(countries_file):
-    '''
-    Read the data from average-latitude-longitude-countries.csv
-    '''
+path = 'D:\\Training\\python-training\\CS101\\'
 
-    path = 'D:\\Training\\python-training\\CS101\\'
+country_names = []
+country_coordinates = []
 
-    country_names = []
-    country_coordinates = []
+# read the file
+countries_file = open(path + 'average-latitude-longitude-countries.csv')
+list_countries_data = countries_file.readlines()
 
-    # read the file
-    countries_file = open(path + countries_file)
-    list_countries_data = countries_file.readlines()
+# loop through each of the records
+# then store them at their respective list of tuples
+for i in range(1, len(list_countries_data)):
+    # get the data at the current index and strip it
+    # this will strip the string with \t \n characters
+    data = list_countries_data[i].strip()
 
-    # loop through each of the records
-    # then store them at their respective list of tuples
-    for i in range(1, len(list_countries_data)):
-        # get the data at the current index and strip it
-        # this will strip the string with \t \n characters
-        data = list_countries_data[i].strip()
- 
-        # split the string using , to transform the data to a list
-        # split it using regular expression to deal with countries that has comma
-        data = re.split(',((?=")|(?=\d)|(?=-))', data)
+    # split the string using , to transform the data to a list
+    # split it using regular expression to deal with countries that has comma
+    data = re.split(',((?=")|(?=\d)|(?=-))', data)
 
-        # remove empty string from the list
-        # strip the " at both ends of each string
-        data = [s.strip('"') for s in data if s!='']
-
-        # create the country (code, name) tuple and append it
-        country_name = (data[0], data[1])
-        country_names.append(country_name)
-
-        # create the country (code, (lat, long)) tuple and append it
-        country_coordinate = (data[0], (float(data[2]), float(data[3])))
-        country_coordinates.append(country_coordinate)
+    # remove empty string from the list
+    # strip the " at both ends of each string
+    data = [s.strip('"') for s in data if s!='']
     
-    return (country_names, country_coordinates)
-        
-# execute read_average_lat_long_countries and unpack the return value
-country_names, country_coordinates = read_average_lat_long_countries('average-latitude-longitude-countries.csv')
-# print(country_names)
-# print(country_coordinates)
+    # create the country (code, name) tuple and append it
+    country_name = (data[0], data[1])
+    country_names.append(country_name)
+    
+    # create the country (code, (lat, long)) tuple and append it
+    country_coordinate = (data[0], (float(data[2]), float(data[3])))
+    country_coordinates.append(country_coordinate)
+
+# print the list that conttains the needed tuples
+print(country_names)
+print(country_coordinates)
 
 # convert the (code, name) tuple to dictionary for easier access of names
 # use dictionary comprehension to accomplish this
