62624db55208d76e65f89144c8cab9c6dc31c20a
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-12-12 01:18:11 +0800

    cleaned up the products.csv

--- CS101/store.py
+++ CS101/store.py
@@ -1,4 +1,17 @@
 import csv
 
-path = 'D:\Training\python-training\CS101'
+path = 'D:\\Training\\python-training\\CS101\\'
 
+def make_product_tuples(products_dir: str):
+    products = []
+
+    with open(products_dir, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+
+        next(reader)
+
+        for product in reader:
+            print(product)
+
+if __name__ == "__main__":
+    make_product_tuples(path + 'products.csv')
