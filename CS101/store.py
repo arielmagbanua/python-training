598cce3ebe8d1c0263fa9007567fe2939eb2d390
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2020-12-12 01:40:17 +0800

    Simplified further the data.

--- CS101/store.py
+++ CS101/store.py
@@ -5,13 +5,19 @@ path = 'D:\\Training\\python-training\\CS101\\'
 def make_product_tuples(products_dir: str):
     products = []
 
-    with open(products_dir, newline='') as csvfile:
+    with open(products_dir, newline='', encoding='utf8') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='|')
 
         next(reader)
 
         for product in reader:
-            print(product)
+            prod_tup = tuple(product)
+            products.append(prod_tup)
+
+    return products
+
+def make_product_objects(products):
+    pass
 
 if __name__ == "__main__":
     make_product_tuples(path + 'products.csv')
