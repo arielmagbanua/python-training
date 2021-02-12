ce1346de3788292720e2ec05ac1d76b39ee99881
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-02-12 22:16:50 +0800

    Added visualization.

---
+++ Courses/PythonForEverybody/capstone/spider.js
@@ -0,0 +1,16 @@
+spiderJson = {"nodes":[
+{"weight":1,"rank":1.9000000000000004, "id":1, "url":"http://www.dr-chuck.com"},
+{"weight":3,"rank":19.0, "id":2, "url":"http://www.dr-chuck.com/dr-chuck/resume/index.htm"},
+{"weight":1,"rank":0.0, "id":3, "url":"http://www.dr-chuck.com/sakai-book"},
+{"weight":1,"rank":1.9000000000000004, "id":8, "url":"http://www.dr-chuck.com/dr-chuck/resume/bio.htm"},
+{"weight":1,"rank":1.9000000000000004, "id":13, "url":"http://www.dr-chuck.com/dr-chuck/resume/leadership.htm"},
+{"weight":1,"rank":1.9000000000000004, "id":15, "url":"http://www.dr-chuck.com/dr-chuck/resume/pictures/index.htm"}],
+"links":[
+{"source":0,"target":1,"value":3},
+{"source":0,"target":2,"value":3},
+{"source":1,"target":0,"value":3},
+{"source":1,"target":3,"value":3},
+{"source":1,"target":4,"value":3},
+{"source":1,"target":5,"value":3},
+{"source":3,"target":1,"value":3},
+{"source":4,"target":1,"value":3}]};
