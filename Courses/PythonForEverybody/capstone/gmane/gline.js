30045b981b458d744e81119acf76db88735a2f48
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-02-13 15:14:03 +0800

    Auto generated files.

---
+++ Courses/PythonForEverybody/capstone/gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Year','umich.edu','ucdavis.edu','ufp.pt','indiana.edu','berkeley.edu','mac.com','columbia.edu','uct.ac.za','unl.edu','mtu.edu'],
+['2005',56,10,10,12,12,12,13,14,12,11],
+['2006',8,9,9,6,4,3,1,0,0,0]
+];
