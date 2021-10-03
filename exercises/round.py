5ed8e10c444d180ccfaaf6dd61ed9b0660b665f7
unknown <magbanua.c.ariel@gmail.com>
2021-10-03 21:59:40 +0800

    Added new examples.

---
+++ exercises/round.py
@@ -0,0 +1,11 @@
+# nearest integer
+print(round(13.56449))
+
+# nearest tenths
+print(round(13.56449, 1))
+
+# nearest hundreths
+print(round(13.56449, 2))
+
+# nearest thousandths
+print(round(13.56449, 3))
