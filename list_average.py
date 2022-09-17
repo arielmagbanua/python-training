547f9306a9fee1d4587e1ba2faab17d8c9e9e2c7
unknown <magbanua.c.ariel@gmail.com>
2022-09-17 21:09:50 +0800

    List average example.

---
+++ list_average.py
@@ -0,0 +1,26 @@
+# input numeric grades in a list sequence
+grades = [int(num) for num in input().split()]
+
+# initialized failing grades to 0
+failing_grades = 0
+
+# counting of failing grades
+for grade in grades:
+    if grade < 72:
+        failing_grades += 1
+
+# get the sum of all grades
+sum_of_grades = sum(grades)
+
+# get the number of grades inputed
+len_of_grades = len(grades)
+
+# getting the average of the numeric grades
+average = sum_of_grades / len_of_grades
+float_average = float(average)
+
+# print the output
+if failing_grades == 0:
+    print(f"Average is: {float_average:.2f}. You didn't fail in any subject.")
+else:
+    print(f"Average is: {float_average:.2f}. You failed in {failing_grades} subjects.")
